b1639c838a0cbce972c18b481da62daae2c7ff6c
Fedesan14 <fede14.sanchez@gmail.com>
2026-07-04 18:35:54 -0300

    first commit

---
+++ src/main/java/com/fedesan14/expin_backend/ExpinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fedesan14.expin_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpinBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fedesan14/expin_backend/ExpinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedesan14.expin_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpinBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
